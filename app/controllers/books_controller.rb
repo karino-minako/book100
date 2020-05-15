94018ad406d6624d780dad27b64a5d2d199e27a2
vagrant <vagrant@localhost.localdomain>
2020-05-15 03:15:34 +0000

    [Review Finish] 5-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+before_action :authenticate_user!, only: [:show, :index, :edit]
+  def top
+  end
+  def about
+  end
+  def index
+  	  @books = Book.all
+  	  @book = Book.new
+  	  @user = current_user
+  end
+  def create
+  	  @book = Book.new(book_params)
+      @book.user_id = current_user.id
+  	  if @book.save
+  	  	 flash[:notice] = 'you have created book successfully'
+  	  	 redirect_to book_path(@book)
+  	  else
+  	  	 @books = Book.all
+  	  	 @user = current_user
+  	  	 render action: :index
+  	  end
+  end
+  def show
+  	  @book = Book.find(params[:id])
+  	  @user = @book.user
+  end
+  def edit
+  	  @book = Book.find(params[:id])
+  	  if current_user.id != @book.user.id
+  	  redirect_to books_path
+  	  end
+  end
+  def update
+  	  @book = Book.find(params[:id])
+  	  if @book.update(book_params)
+  	  	 flash[:notice] = 'you have updated book successfully'
+  	     redirect_to book_path(@book)
+  	  else
+  	  	 render action: :edit
+  	  end
+  end
+  def destroy
+  	  @book = Book.find(params[:id])
+  	  @book.destroy
+  	  flash[:notice] = 'Book was successfully destroyed'
+  	  redirect_to books_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'books#top'
+  devise_for :users
+
+
+  get 'home/about' => "books#about"
+
+  resources :books
+
+  resources :users
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
